7f3c2274a50ca4e95646c4e54be78ccbf9d33ed1
DAVENJAGI <njagidave39@gmail.com>
2024-03-25 02:44:52 +0300

    minOperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Minimum operations
+Minimum Operations
 """
+
 
 def minOperations(n):
     """
-    calculate minimum operations
+    Calculates minimum operations
     """
     if n <= 1:
         return 0
-    for num in range(1, n+1):
+    for num in range(2, n+1):
         if n % num == 0:
             return minOperations(int(n/num)) + num
-
